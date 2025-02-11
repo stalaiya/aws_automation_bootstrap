e78444b6f47f7c0433a993b814784ee187ebbef3
Shayan Talaiyan <shayan.talaiyan@ibm.com>
2025-02-11 15:34:35 -0500

    first workflow

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <deploy|update|delete>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <deploy|update|delete> <stack_name> <cf_template_file>"
     exit 1
 fi
 # Assign the argument to a variable
 action=$1
-# Define the CloudFormation template file and stack name
-template_file="cloudFormation/bootstrap.yaml"
-stack_name="awstfbootstrap"
+stack_name=$2
+cf_template_file=$3
 
 # Check if the action is "deploy"
 if [ "$action" == "deploy" ]; then
     echo "Deploying CloudFormation stack..."
     # Deploy the CloudFormation stack using the AWS CLI
-    aws cloudformation create-stack --stack-name $stack_name --template-body file://$template_file --capabilities CAPABILITY_NAMED_IAM
+    aws cloudformation create-stack --stack-name $stack_name --template-body file://$cf_template_file --capabilities CAPABILITY_NAMED_IAM
     # Wait for the stack creation to complete
     aws cloudformation wait stack-create-complete --stack-name $stack_name
     echo "Stack deployment completed successfully."
@@ -31,7 +30,7 @@ elif [ "$action" == "delete" ]; then
 elif [ "$action" == "update" ]; then
     echo "Updating CloudFormation stack..."
     # Update the CloudFormation stack using the AWS CLI
-    aws cloudformation update-stack --stack-name $stack_name --template-body file://$template_file --capabilities CAPABILITY_NAMED_IAM
+    aws cloudformation update-stack --stack-name $stack_name --template-body file://$cf_template_file --capabilities CAPABILITY_NAMED_IAM
     # Wait for the stack update to complete
     aws cloudformation wait stack-update-complete --stack-name $stack_name
     echo "Stack update completed successfully."
